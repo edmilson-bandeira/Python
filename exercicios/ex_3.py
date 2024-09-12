115feee8d90134fdfa277c074ebb6d0d07faee56
Edmilson Bandeira <111466898+edmilson-bandeira@users.noreply.github.com>
2024-09-12 18:45:45 +0100

    ex_3

--- exercicios/ex_3.py
+++ exercicios/ex_3.py
@@ -3,3 +3,4 @@ livro = input("Qual é o seu livro favorito?")
 musica = input("Qual a sua música favorita?")
 
 print("Seu filme favorito é " + filme +", seu livro favorito é " + livro + "e sua música favorita é " + musica + ".")
+
