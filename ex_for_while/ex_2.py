877afbcfdcc2553530c983343c807e112e5d20f9
Edmilson Bandeira <111466898+edmilson-bandeira@users.noreply.github.com>
2024-09-16 13:32:31 +0000

    up_ex_2

--- ex_for_while/ex_2.py
+++ ex_for_while/ex_2.py
@@ -3,14 +3,14 @@ import random
 num_Alet = random.randint(1, 50)
 
 max_tentativas = 5
-tent_restantes = max_tentativas
 
 print("adivinha o numero")
+print("Tentativas máxima: " + str(max_tentativas))
 
-while tent_restantes > 0:
+while max_tentativas > 0:
     palpite = int(input("Digite o seu palpite: "))
     if palpite == num_Alet:
-        print("Parabés! Acertaste o número secreto")
+        print("Parabéns! Acertaste o número secreto! :)")
         break
     elif palpite < num_Alet:
          print("O número secreto é maior.")
@@ -18,5 +18,7 @@ while tent_restantes > 0:
         palpite > num_Alet
         print("O número secreto é menor.")
 
-    tent_restantes -= 1
-    print("Tentativas restantes: " + str(tent_restantes))
+    max_tentativas -= 1
+    print("Tentativas restantes: " + str(max_tentativas))
+
+print("O jogo terminou!")
