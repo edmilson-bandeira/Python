56262f0a7dc362342cf1b96edc2e818d6a61eb8d
Edmilson Bandeira <111466898+edmilson-bandeira@users.noreply.github.com>
2024-09-16 11:09:02 +0000

    ex_1 done

--- ex_for_while/ex_1.py
+++ ex_for_while/ex_1.py
@@ -1,6 +1,15 @@
 n = int(input("insere o total de notas: "))
 notas = []
 
-while n <=10 :
-    notas = int(input("insere a nota: "))
-    
+controller = 0
+
+while controller <= n :
+    nota = int(input("insere a nota: "))
+    if (nota < 0 or nota > 20):
+        print("nota inválida :(")
+    else: 
+        notas.append(nota)
+        media = sum(notas) / len(notas)
+        print("média: " + str(media))
+    controller+=1
+
