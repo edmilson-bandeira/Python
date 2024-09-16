01648d49540ac596fe66d2e2524bcbde38651d2d
Edmilson Bandeira <111466898+edmilson-bandeira@users.noreply.github.com>
2024-09-16 21:12:13 +0000

    up

--- ex_for_while/ex_1.py
+++ ex_for_while/ex_1.py
@@ -1,34 +1,35 @@
+
 n = int(input("insere o total de notas: "))
-# notas = []
+notas = []
+media = 0
 
-# n = int(input("insere o total de notas: "))
-# # notas = []
-# # media = 0
+controller = 1
 
-# # controller = 1
-
-# # while controller <= n :
-# #     nota = int(input("insere a nota: "))
-# #     if (nota < 0 or nota > 20):
-# #         print("nota inválida :(")
-# #         controller = 0
-# #     else: 
-# #         notas.append(nota)
-# #         media = sum(notas) / len(notas)
-# #         round(media, 2)
-# #     controller+=1   
-
-# # print("média: " + str(media))
-
-
-
-
-notas = 0
-for nota in range(n):
+while controller <= n :
     nota = int(input("insere a nota: "))
     if (nota < 0 or nota > 20):
-       print("nota inválida :(")
-    else:
-        # soma_notas = notas + nota
-        media = notas * n
+        print("nota inválida :(. insira um nota de 0 a 20")
+        controller = 0
+    else: 
+        notas.append(nota)
+        media = sum(notas) / len(notas)
+        round(media, 2)
+    controller+=1   
+
 print("média: " + str(media))
+
+
+
+#outra opção com for
+# soma_notas = 0
+# for nota in range(n):
+#     nota = int(input("insere a nota: "))
+#     if (nota < 0 or nota > 20):
+#        print("nota inválida :(. insira um nota de 0 a 20")
+#     else:
+#         soma_notas += nota
+# media = soma_notas / n
+# print("média: " + str(media))
+
+
+
